c0cbf287dbd66ef5880d58d8e924e6ceec23551b
ehdals6034 <ehdals6034@naver.com>
2022-06-14 02:45:59 +0000

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /ur5robot_ws/src
+# Build directory: /ur5robot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robotiq_moveit")
+subdirs("ur5/ur5_description")
+subdirs("ur5/ur5_gazebo")
+subdirs("robotiq/robotiq_description")
---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/ur5robot_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/ur5robot_ws/devel;/ros_ws/devel;/opt/ros/melodic'.split(';') if '/ur5robot_ws/devel;/ros_ws/devel;/opt/ros/melodic' != '' else []
